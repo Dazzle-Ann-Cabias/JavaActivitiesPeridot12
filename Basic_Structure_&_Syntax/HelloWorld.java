ad7bdfcced60fef26023fea33c948ee36aaeb393
Dazzle-Ann-Cabias <dazzelanncabias@gmail.com>
2026-06-17 14:32:06 +0800

    main

--- Basic_Structure_&_Syntax/HelloWorld.java
+++ Basic_Structure_&_Syntax/HelloWorld.java
@@ -1,12 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("I'm dazzel Ann C. cabias"); 
+        System.out.println("13678923849l");
+        System.out.println("I'm 17 years old.");
+        System.out.println("I live at Caloocan City");
+        System.out.println("Addition5 + 3 = " + (5 + 3));
+        System.out.println("Subtraction10 - 4 = " + (10 - 4));
+        System.out.println("Multiplication6 * 7 = " + (6 * 7));
+        System.out.println("Division20 / 5 = " + (20 / 5));
+        System.out.println("Modulus10 % 3 = " + (10 % 3));
+        System.out.println("Exponentiation2 ^ 3 = " + Math.pow(2, 3));
+        System.out.println("Increment5++ = " + (5 + 1));
+        System.out.println("Decrement10-- = " + (10 - 1));
     }
-}
-
-
-class MAIN {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+} 
