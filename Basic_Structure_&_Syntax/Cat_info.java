a05ef7f762eea86bd72b3b686af7185bd448fcc3
Dazzling-Potato <dazzelanncabias@gmail.com>
2026-06-18 14:55:10 +0800

    Add files via upload

---
+++ Basic_Structure_&_Syntax/Cat_info.java
@@ -0,0 +1,81 @@
+
+
+public class Cat_info {
+    public static void main(String[] args) {
+    //====================================================//
+    String catName = "Sky";
+    String catName2 = "Maple";
+    String catName3 = "Fluffy";
+    String catName4 = "Whiskers"; 
+    String catName5 = "Mittens";
+    String catName6 = "Shadow";
+    String catName7 = "Simba";
+    String catName8 = "Luna";  
+    //===================================================//
+    String catBreed = "Siamese";
+    String catBreed2 = "Persian";
+    String catBreed3 = "Maine Coon";
+    String catBreed4 = "Ragdoll";
+    String catBreed5 = "Bengal";
+    String catBreed6 = "Sphynx";
+    String catBreed7 = "Scottish Fold";
+    String catBreed8 = "British Shorthair";
+    //===================================================// 
+    int catID = 15;
+    int catID2 = 20;
+    int catID3 = 10;
+    int catID4 = 5;
+    int catID5 = 15;
+    int catID6 = 20;
+    int catID7 = 10;
+    int catID8 = 5;
+    //===================================================//
+    int catAge = 23;
+    float catFee = 75.25f;
+    char catGrade = 'B';
+    String allCatInfo = catName + " " + catID + " " + catAge + " " + catFee + " " + catGrade;
+    String allCatInfo2 = catName2 + " " + catID2 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed2;
+    String allCatInfo3 = catName3 + " " + catID3 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed3;
+    String allCatInfo4 = catName4 + " " + catID4 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed4;
+    String allCatInfo5 = catName5 + " " + catID5 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed5;
+    String allCatInfo6 = catName6 + " " + catID6 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed6;
+    String allCatInfo7 = catName7 + " " + catID7 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed7;
+    String allCatInfo8 = catName8 + " " + catID8 + " " + catAge + " " + catFee + " " + catGrade + " " + catBreed8;
+
+    //====================================================//
+    System.out.println("Cat name: " + catName);
+    System.err.println("Cat name2: " + catName2);
+    System.out.println("Cat name3: " + catName3);
+    System.out.println("Cat name4: " + catName4);
+    System.out.println("Cat name5: " + catName5);
+    System.out.println("Cat name6: " + catName6);   
+    System.out.println("Cat name7: " + catName7);
+    System.out.println("Cat name8: " + catName8);  
+    //====================================================//
+    System.out.println("Cat id: " + catID);
+    System.out.println("Cat id: " + catID2);
+    System.out.println("Cat id: " + catID3);
+    System.out.println("Cat age: " + catAge);
+    System.out.println("Cat fee: " + catFee);
+    System.out.println("Cat grade: " + catGrade);
+    //=================summarization============================//
+    System.out.println("All cat info: " + allCatInfo);
+    System.out.println("All cat info2: " + allCatInfo2);
+    System.out.println("All cat info3: " + allCatInfo3);
+    System.out.println("All cat info: " + allCatInfo4);
+    System.out.println("All cat info: " + allCatInfo5);
+    System.out.println("All cat info: " + allCatInfo6);
+    System.out.println("All cat info: " + allCatInfo7);
+    System.out.println("All cat info: " + allCatInfo8);
+    //===========breed=========================================//
+    System.out.println("Cat breed: " + catBreed);
+    System.out.println("Cat breed2: " + catBreed2);
+    System.out.println("Cat breed3: " + catBreed3);
+    System.out.println("Cat breed4: " + catBreed4);
+    System.out.println("Cat breed5: " + catBreed5);
+    System.out.println("Cat breed6: " + catBreed6);
+    System.out.println("Cat breed7: " + catBreed7);
+    System.out.println("Cat breed8: " + catBreed8);
+    // BAT AYAM MA COMMIT! WAIT SECOND TRY//
+    }
+}
